bd15663305ea48de963aaee1e8a12dd5413fc707
hamadayuuki <yuukirinnma@gmail.com>
2023-06-25 19:07:15 +0900

    add: init project

---
+++ SwapWithMe/SwapWithMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwapWithMeApp.swift
+//  SwapWithMe
+//
+//  Created by 濵田　悠樹 on 2023/06/25.
+//
+
+import SwiftUI
+
+@main
+struct SwapWithMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
