1ff69b224533bc2b7103211a7c21e92458f190fc
hamadayuuki <yuukirinnma@gmail.com>
2023-07-04 20:31:28 +0900

    Add View to Feature module

--- Package.swift
+++ Package.swift
@@ -5,13 +5,17 @@ import PackageDescription
 
 let package = Package(
     name: "SwapWithMe",
+    // 対象とする Minバージョンを指定
+    platforms: [.iOS(.v13)],
     // モジュール の依存関係を定義
     products: [
+        .library(name: "Feature", targets: ["Feature"])
     ],
     // ライブラリ の依存関係を定義
     dependencies: [
     ],
     // target や test 用の target を追加していく
     targets: [
+        .target(name: "Feature")
     ]
 )
