20fd751cbd1cd58c3b19e3def7aa085942912f0e
hamadayuuki <yuukirinnma@gmail.com>
2023-07-08 20:38:36 +0900

    Update using firebase

--- App/SwapWithMe/SwapWithMeApp.swift
+++ App/SwapWithMe/SwapWithMeApp.swift
@@ -7,12 +7,25 @@
 
 import SignUp
 import SwiftUI
+import FirebaseCore
 
 @main
 struct SwapWithMeApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
             SignUpView()
         }
     }
+}
+
+// MARK: - AppDelegate
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
 }
