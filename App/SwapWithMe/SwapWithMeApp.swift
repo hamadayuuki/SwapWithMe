52d0e69a0ca5c98191ac8ae1c80790c01c345476
hamadayuuki <yuukirinnma@gmail.com>
2023-07-08 14:57:09 +0900

    Update using SignUpView

--- App/SwapWithMe/SwapWithMeApp.swift
+++ App/SwapWithMe/SwapWithMeApp.swift
@@ -5,14 +5,14 @@
 //  Created by 濵田　悠樹 on 2023/06/25.
 //
 
-import Feature
+import SignUp
 import SwiftUI
 
 @main
 struct SwapWithMeApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SignUpView()
         }
     }
 }
