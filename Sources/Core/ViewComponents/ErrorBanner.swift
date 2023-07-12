a6a9d48617a83611d306b26a0206910563d5f684
hamadayuuki <yuukirinnma@gmail.com>
2023-07-13 00:17:55 +0900

    Add Error Banner

--- Sources/Core/ViewComponents/ErrorBanner.swift
+++ Sources/Core/ViewComponents/ErrorBanner.swift
@@ -7,14 +7,25 @@
 
 import SwiftUI
 
-struct ErrorBanner: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+public struct ErrorBanner: View {
+    private(set) var errorTitle = ""
+
+    public init(errorTitle: String) {
+        self.errorTitle = errorTitle
+    }
+
+    public var body: some View {
+        Text("\(errorTitle)")
+            .foregroundColor(.white)
+            .font(.system(size: 16, weight: .bold, design: .rounded))
+            .frame(maxWidth: .infinity, maxHeight: 60)
+            .background(.red.opacity(0.9))
+            .cornerRadius(10.0)
     }
 }
 
 struct ErrorBanner_Previews: PreviewProvider {
     static var previews: some View {
-        ErrorBanner()
+        ErrorBanner(errorTitle: "エラーが発生しました")
     }
 }
