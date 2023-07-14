d8e8c5282e21c8fa848e4c65580b02217cae61cc
hamadayuuki <yuukirinnma@gmail.com>
2023-07-15 03:12:21 +0900

    Clean code with swift-format

--- App/SwapWithMe/SwapWithMeApp.swift
+++ App/SwapWithMe/SwapWithMeApp.swift
@@ -23,19 +23,17 @@ struct SwapWithMeApp: App {
 // MARK: - AppDelegate
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
         FirebaseApp.configure()
         return true
     }
 
     /// 電話番号認証
     /// サイレントプッシュ通知を受け取る
-    func  application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
         if Auth.auth().canHandleNotification(userInfo) {
             completionHandler(.noData)
             return
         }
     }
 }
-
-
