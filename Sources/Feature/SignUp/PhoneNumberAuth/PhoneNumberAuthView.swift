f4777aa5d6fc6d0e864d222c9f5e5c8164309232
hamadayuuki <yuukirinnma@gmail.com>
2023-07-17 15:35:02 +0900

    Update view using TCA

--- Sources/Feature/SignUp/PhoneNumberAuth/PhoneNumberAuthView.swift
+++ Sources/Feature/SignUp/PhoneNumberAuth/PhoneNumberAuthView.swift
@@ -5,15 +5,16 @@
 //  Created by 濵田　悠樹 on 2023/07/08.
 //
 
+import ComposableArchitecture
 import FirebaseAuth
 import PopupView
 import SwiftUI
 import ViewComponents
 
 struct PhoneNumberAuthView: View {
+    let store: StoreOf<PhoneNumberAuth>
+
     @State private var phoneNumber = ""
-    @State private var isShowConfirmVerificationCodeView = false
-    @State private var isErrorBanner = false
 
     private var isButtonEnable: Bool {
         if phoneNumber.count == 11 {
@@ -29,76 +30,92 @@ struct PhoneNumberAuthView: View {
     }
 
     var body: some View {
-        VStack(spacing: 18) {
-            VStack(alignment: .leading, spacing: 18) {
-                Text("電話番号を使用して登録")
-                    .font(.system(size: 24, weight: .bold, design: .rounded))
-
-                Text("間違った電話番号を登録すると、メッセージが届かずユーザー登録できません。")
-                    .font(.system(size: 12, weight: .regular, design: .rounded))
-
-                TextField("000-0000-0000", text: $phoneNumber)
-                    .font(.system(size: 32, weight: .bold, design: .rounded))
-                    .padding(.top, 40)
-                    .keyboardType(.numberPad)
-
-                Divider()
-                    .frame(height: 5)
-                    .background(.gray)
-            }
-
-            Button(
-                action: {
-                    fetchSMS()
-                },
-                label: {
-                    Text("次へ")
-                        .foregroundColor(.white)
+        WithViewStore(self.store, observe: { $0 }) { viewStore in
+            VStack(spacing: 18) {
+                VStack(alignment: .leading, spacing: 18) {
+                    Text("電話番号を使用して登録")
                         .font(.system(size: 24, weight: .bold, design: .rounded))
-                        .frame(maxWidth: .infinity, maxHeight: 50)
-                        .background(transButtonBackground)
-                        .cornerRadius(10)
-                }
-            )
-            .padding(.top, 40)
-            .disabled(!isButtonEnable)
 
-            NavigationLink(destination: ConfirmVerificationCodeView(), isActive: $isShowConfirmVerificationCodeView) {
-                EmptyView()
+                    Text("間違った電話番号を登録すると、メッセージが届かずユーザー登録できません。")
+                        .font(.system(size: 12, weight: .regular, design: .rounded))
+
+                    TextField("000-0000-0000", text: $phoneNumber)
+                        .font(.system(size: 32, weight: .bold, design: .rounded))
+                        .padding(.top, 40)
+                        .keyboardType(.numberPad)
+
+                    Divider()
+                        .frame(height: 5)
+                        .background(.gray)
+                }
+
+                Button(
+                    action: {
+                        viewStore.send(.tappedSMSButton(phoneNumber))
+                    },
+                    label: {
+                        Text("次へ")
+                            .foregroundColor(.white)
+                            .font(.system(size: 24, weight: .bold, design: .rounded))
+                            .frame(maxWidth: .infinity, maxHeight: 50)
+                            .background(transButtonBackground)
+                            .cornerRadius(10)
+                    }
+                )
+                .padding(.top, 40)
+                .disabled(!isButtonEnable)
+
+                NavigationLink(
+                    destination: ConfirmVerificationCodeView(),
+                    isActive: viewStore.binding(
+                        get: { $0.isShowConfirmVerificationCodeView },
+                        send: .bindingIsShowConfirmVerificationCodeView(viewStore.isShowConfirmVerificationCodeView)
+                    )
+                ) {
+                    EmptyView()
+                }
             }
-        }
-        .frame(maxHeight: .infinity, alignment: .top)
-        .fitToReadableContentGuide()
-        .padding(.top, 60)
-        .popup(isPresented: $isErrorBanner) {
-            ErrorBanner(errorTitle: "電話番号の認証に失敗しました")
-                .fitToReadableContentGuide()
-        } customize: {
-            $0
-                .type(.floater())
-                .position(.bottom)
-                .animation(.spring())
-                .closeOnTapOutside(true)
-                .backgroundColor(.black.opacity(0.3))
+            .frame(maxHeight: .infinity, alignment: .top)
+            .fitToReadableContentGuide()
+            .padding(.top, 60)
+            .popup(
+                isPresented: viewStore.binding(
+                    get: { $0.isErrorBanner },
+                    send: .bindingIsErrorBanner(viewStore.isErrorBanner)
+                )
+            ) {
+                ErrorBanner(errorTitle: "電話番号の認証に失敗しました")
+                    .fitToReadableContentGuide()
+            } customize: {
+                $0
+                    .type(.floater())
+                    .position(.bottom)
+                    .animation(.spring())
+                    .closeOnTapOutside(true)
+                    .backgroundColor(.black.opacity(0.3))
+            }
         }
     }
 
-    private func fetchSMS() {
-        // Firebase Auth のテストデータとして私用の電話番号を設定済み
-        PhoneAuthProvider.provider()
-            .verifyPhoneNumber("+81" + phoneNumber, uiDelegate: nil) { verificationID, error in
-                if let error = error {
-                    isErrorBanner = true
-                    return
-                }
-                UserDefaults.standard.set(verificationID, forKey: "verificationID")
-                isShowConfirmVerificationCodeView = true
-            }
-    }
+    //    private func fetchSMS() {
+    //        // Firebase Auth のテストデータとして私用の電話番号を設定済み
+    //        PhoneAuthProvider.provider()
+    //            .verifyPhoneNumber("+81" + phoneNumber, uiDelegate: nil) { verificationID, error in
+    //                if let error = error {
+    //                    isErrorBanner = true
+    //                    return
+    //                }
+    //                UserDefaults.standard.set(verificationID, forKey: "verificationID")
+    //                isShowConfirmVerificationCodeView = true
+    //            }
+    //    }
 }
 
 struct PhoneNumberAuthView_Previews: PreviewProvider {
     static var previews: some View {
-        PhoneNumberAuthView()
+        PhoneNumberAuthView(
+            store: Store(initialState: PhoneNumberAuth.State()) {
+                PhoneNumberAuth()
+            })
     }
 }
