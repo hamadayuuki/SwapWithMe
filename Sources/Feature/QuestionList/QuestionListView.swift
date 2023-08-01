f08fb5103a07eef469651b4594c2f9d5595a598c
hamadayuuki <yuukirinnma@gmail.com>
2023-08-01 23:19:46 +0900

    Update show partner card

--- Sources/Feature/QuestionList/QuestionListView.swift
+++ Sources/Feature/QuestionList/QuestionListView.swift
@@ -9,14 +9,58 @@ import ReadabilityModifier
 import SwiftUI
 
 public struct QuestionListView: View {
+    @State var cardImage: Image = Image(uiImage: UIImage())
 
     public init() {}
 
     public var body: some View {
         VStack(spacing: 32) {
+            ZStack {
+                card()
+                partnerInfo(name: "ながのめいですす", age: 24, affiliation: "フレンドリー")
+                    .offset(x: 0, y: 400 * 0.7 * 0.25)
+            }
             questionText(questionNum: 1)
         }
         .fitToReadableContentGuide()
+        .onAppear {
+            cardImage = Image("nagano")
+        }
+    }
+
+    private func card() -> some View {
+        ZStack {
+            cardImage
+                .resizable()
+                .scaledToFill()
+
+            LinearGradient(
+                gradient: Gradient(colors: [.clear, .black.opacity(0.7)]),
+                startPoint: .top,
+                endPoint: .bottom
+            )
+        }
+        .frame(width: 250 * 0.7, height: 400 * 0.7)
+        .cornerRadius(20)
+        .overlay(
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(Color.green, lineWidth: 4)
+        )
+    }
+
+    private func partnerInfo(name: String, age: Int, affiliation: String) -> some View {
+        VStack(spacing: 6) {
+            Text("\(name)")
+                .font(.system(size: 18, weight: .bold, design: .rounded))
+
+            HStack(spacing: 6) {
+                Text("\(age)歳")
+
+                Text("\(affiliation)")
+            }
+            .font(.system(size: 12, weight: .medium, design: .rounded))
+        }
+        .foregroundColor(.white)
     }
 
     private func questionText(questionNum: Int) -> some View {
