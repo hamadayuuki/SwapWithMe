043d31f8aa361d3ed1717ab75dc485782f49fbf9
hamadayuuki <yuukirinnma@gmail.com>
2023-08-04 00:49:06 +0900

    Update layout

--- Sources/Feature/PartnerCards/PartnerCardsView.swift
+++ Sources/Feature/PartnerCards/PartnerCardsView.swift
@@ -5,6 +5,7 @@
 //  Created by 濵田　悠樹 on 2023/08/03.
 //
 
+import ReadabilityModifier
 import SwiftUI
 
 public struct PartnerCardsView: View {
@@ -12,7 +13,21 @@ public struct PartnerCardsView: View {
     public init() {}
 
     public var body: some View {
-        Text("PartnerCardsView")
+        ScrollView {
+            VStack(spacing: 18) {
+                ForEach(0..<5) { i in
+                    HStack(spacing: 12) {
+                        ForEach(0..<3) { i in
+                            Text("Card")
+                                .frame(width: 250 * 0.42, height: 400 * 0.42)
+                                .background(.gray.opacity(0.5))
+                                .cornerRadius(20)
+                        }
+                    }
+                }
+            }
+            .fitToReadableContentGuide()
+        }
     }
 }
 
