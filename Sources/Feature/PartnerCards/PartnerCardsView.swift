6390eea3ec7a77e1be00bc5f0a5a838b3ce6d4a5
hamadayuuki <yuukirinnma@gmail.com>
2023-08-04 01:00:30 +0900

    Update show partner card

--- Sources/Feature/PartnerCards/PartnerCardsView.swift
+++ Sources/Feature/PartnerCards/PartnerCardsView.swift
@@ -9,6 +9,7 @@ import ReadabilityModifier
 import SwiftUI
 
 public struct PartnerCardsView: View {
+    @State var cardImage: Image = Image(uiImage: UIImage())
 
     public init() {}
 
@@ -18,16 +19,55 @@ public struct PartnerCardsView: View {
                 ForEach(0..<5) { i in
                     HStack(spacing: 12) {
                         ForEach(0..<3) { i in
-                            Text("Card")
-                                .frame(width: 250 * 0.42, height: 400 * 0.42)
-                                .background(.gray.opacity(0.5))
-                                .cornerRadius(20)
+                            partnerCard()
                         }
                     }
                 }
             }
             .fitToReadableContentGuide()
         }
+        .onAppear {
+            cardImage = Image("nagano")
+        }
+    }
+
+    private func partnerCard() -> some View {
+        ZStack {
+            card()
+            partnerInfo(name: "ながのめいですす", age: 24, affiliation: "フレンドリー")
+                .offset(x: 0, y: 400 * 0.42 * 0.25)
+        }
+    }
+
+    private func card() -> some View {
+        ZStack {
+            cardImage
+                .resizable()
+                .scaledToFill()
+
+            LinearGradient(
+                gradient: Gradient(colors: [.clear, .black.opacity(0.7)]),
+                startPoint: .top,
+                endPoint: .bottom
+            )
+        }
+        .frame(width: 250 * 0.42, height: 400 * 0.42)
+        .cornerRadius(20)
+    }
+
+    private func partnerInfo(name: String, age: Int, affiliation: String) -> some View {
+        VStack(spacing: 6) {
+            Text("\(name)")
+                .font(.system(size: 12, weight: .bold, design: .rounded))
+
+            HStack(spacing: 6) {
+                Text("\(age)歳")
+
+                Text("\(affiliation)")
+            }
+            .font(.system(size: 8, weight: .medium, design: .rounded))
+        }
+        .foregroundColor(.white)
     }
 }
 
