beeb72b46ca41710875d21ea616115d723f7ef63
hamadayuuki <yuukirinnma@gmail.com>
2023-08-06 16:46:14 +0900

    Add tab view

---
+++ Sources/Feature/Tab/AppTabView.swift
@@ -0,0 +1,35 @@
+//
+//  AppTabView.swift
+//
+//
+//  Created by 濵田　悠樹 on 2023/08/06.
+//
+
+import Home
+import PartnerCards
+import SwiftUI
+
+public struct AppTabView: View {
+
+    public init() {}
+
+    public var body: some View {
+        TabView {
+            PartnerCardsView()
+                .tabItem {
+                    Label("Home", systemImage: "house")
+                }
+
+            HomeView()
+                .tabItem {
+                    Label("Search", systemImage: "magnifyingglass")
+                }
+        }
+    }
+}
+
+struct AppTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppTabView()
+    }
+}
