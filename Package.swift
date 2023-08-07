aa482fba3bb21ea970cac80ebf3d8dd2f0f5b9fd
hamadayuuki <yuukirinnma@gmail.com>
2023-08-08 02:02:29 +0900

    Install FirebaseStorage

--- Package.swift
+++ Package.swift
@@ -24,6 +24,7 @@ let analytics: TargetDependency = .product(name: "FirebaseAnalytics", package: "
 let fireAuth: TargetDependency = .product(name: "FirebaseAuth", package: "firebase-ios-sdk")
 let fireStore: TargetDependency = .product(name: "FirebaseFirestore", package: "firebase-ios-sdk")
 let fireStoreSwift: TargetDependency = .product(name: "FirebaseFirestoreSwift", package: "firebase-ios-sdk")
+let fireStorage: TargetDependency = .product(name: "FirebaseStorage", package: "firebase-ios-sdk")
 let popupView: TargetDependency = .product(name: "PopupView", package: "PopupView")
 let composableArchitecture: TargetDependency = .product(name: "ComposableArchitecture", package: "swift-composable-architecture")
 let dependencies: TargetDependency = .product(name: "Dependencies", package: "Dependencies")
