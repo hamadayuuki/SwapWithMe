daf097e9a8507f4a9741ace1d47cf098484076df
hamadayuuki <yuukirinnma@gmail.com>
2023-08-10 00:57:40 +0900

    Remove unusing code

--- Sources/Feature/Search/PartnerSearchView.swift
+++ Sources/Feature/Search/PartnerSearchView.swift
@@ -10,22 +10,8 @@ import Request
 import SwiftUI
 import User
 
-private struct Partner: Hashable {
-    var uid: Int
-    var iconURL: String
-    var name: String
-    var description: String
-}
-
 public struct PartnerSearchView: View {
     @State private var searchText = ""
-    private var partners: [Partner] = [
-        .init(uid: 0, iconURL: "kiyohara", name: "きよはらしょうう", description: "こんにちは。きよはらしょうです。モデルをやっています。"),
-        .init(uid: 1, iconURL: "nagano", name: "ながのめいです", description: "こんばんは！。ながのめいです。女優をやっています。"),
-        .init(uid: 2, iconURL: "narita", name: "なりたりょう", description: "どうも！モデルをやっています。短髪です。"),
-        .init(uid: 3, iconURL: "hotta", name: "ほったまゆですす", description: "ども！笑顔が素敵と言われます。よろしくお願いしますっ！"),
-        .init(uid: 4, iconURL: "nagano2", name: "ながのめいで2", description: "こんにちは。ながのめいです。女優をやっています。２"),
-    ]
     @State private var users: [User] = []
 
     public init() {}
