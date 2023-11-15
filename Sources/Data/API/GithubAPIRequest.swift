cd16f6ae79476dc8c79d8087659c5b52e753895f
hamadayuuki <yuukirinnma@gmail.com>
2023-11-15 22:13:30 +0900

    Add GithubAPIRequest for search repository

---
+++ Sources/Data/API/GithubAPIRequest.swift
@@ -0,0 +1,40 @@
+//
+//  GithubAPIRequest.swift
+//
+//
+//  Created by 濵田　悠樹 on 2023/11/15.
+//
+
+import Foundation
+
+
+
+public final class GithubAPIRequest {
+    private let baseURL = URL(string: "https://api.github.com")!
+
+    // MARK: - SearchRepository
+    
+    /// let request = GIthubAPI.SearchRepository(searchWord: "swift")
+    public struct SearchRepository: APIRequest {
+        let searchWord: String
+
+        public typealias Response: SearchRepositoryResponse
+
+        public var path: String {
+            return "/search/repositories"
+        }
+
+        public var method: HTTPMethod {
+            return .get
+        }
+
+        public var urlQueryItem: URLQueryItem {
+            return [URLQueryItem(name: "q", value: searchWord)]
+        }
+    }
+
+    // TODO: - 別ファイルにて定義
+    public struct SearchRepositoryResponse: Decodable {
+        let totalCount: Int
+    }
+}
