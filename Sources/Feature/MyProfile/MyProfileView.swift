e7b216bd5045fdd1f52644daa4f0cb8df2be0f92
hamadayuuki <yuukirinnma@gmail.com>
2024-02-08 19:56:47 +0900

    Add assets to MyProfileView

--- Sources/Feature/MyProfile/MyProfileView.swift
+++ Sources/Feature/MyProfile/MyProfileView.swift
@@ -5,6 +5,7 @@
 //  Created by 濵田　悠樹 on 2024/02/08.
 //
 
+import ReadabilityModifier
 import SwiftUI
 import User
 
@@ -28,10 +29,10 @@ public struct MyProfileView: View {
 
         var icon: Image {
             switch self {
-            case .twitter: return Image("hotta2")
-            case .instagram: return Image("hotta3")
-            case .line: return Image("hotta4")
-            case .other(_): return Image("hotta5")
+            case .twitter: return Image("x")
+            case .instagram: return Image("instagram")
+            case .line: return Image("line")
+            case .other(_): return Image("")
             }
         }
     }
@@ -67,68 +68,79 @@ public struct MyProfileView: View {
     public init() {}
 
     public var body: some View {
-        VStack(spacing: 24) {
-            // アイコン
-            Image("hotta")
-                .resizable()
-                .scaledToFill()
-                .frame(width: 150, height: 150)
-                .cornerRadius(75)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 75).stroke(Color.black, lineWidth: 4)
-                )
+        ScrollView {
+            VStack(spacing: 24) {
+                // アイコン
+                Image("hotta")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 150, height: 150)
+                    .cornerRadius(75)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 75).stroke(Color.black, lineWidth: 4)
+                    )
 
-            // ユーザー情報
-            VStack(spacing: 12) {
-                Text("@hotta_mayu")
-                    .font(.system(size: 24, weight: .bold, design: .rounded))
-                Text("hello, my name is hotta mayu")
-                    .font(.system(size: 16, weight: .regular, design: .rounded))
-                    .foregroundStyle(.gray)
-            }
-
-            // SNSアイコン
-            HStack(spacing: 12) {
-                ForEach(mySns, id: \.self) { sns in
-                    sns.icon
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 50, height: 50)
-                        .cornerRadius(25)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 25).stroke(Color.black, lineWidth: 1)
-                        )
+                // ユーザー情報
+                VStack(spacing: 12) {
+                    Text("@hotta_mayu")
+                        .font(.system(size: 24, weight: .bold, design: .rounded))
+                    Text("hello, my name is hotta mayu")
+                        .font(.system(size: 16, weight: .regular, design: .rounded))
+                        .foregroundStyle(.gray)
                 }
-            }
-            .frame(maxWidth: .infinity)
 
-            // フォロー, フォロワー, やっほう数
-            HStack(spacing: 24) {
-                ForEach(myStatus, id: \.self) { status in
-                    VStack(spacing: 4) {
-                        Text("\(status.num)")
-                            .font(.system(size: 20, weight: .bold, design: .rounded))
-                        Text(status.title)
-                            .font(.system(size: 12, weight: .regular, design: .rounded))
-                    }
-                    if status == .follower || status == .following {
-                        Divider()
-                    }
-                }
-            }
-            .frame(maxHeight: 50)
-
-            VStack(spacing: 12) {
-                Text("最近交換したユーザー")
-                    .font(.system(size: 24, weight: .bold, design: .rounded))
-
+                // SNSアイコン
                 HStack(spacing: 12) {
-                    partnerCard(cardImage: Image(""), partner: PartnerInfo(name: "hotta mayu", age: 25, personality: "cute"))
+                    ForEach(mySns, id: \.self) { sns in
+                        sns.icon
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 50, height: 50)
+                            .cornerRadius(25)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 25).stroke(Color.black, lineWidth: 1)
+                            )
+                    }
                 }
+                .frame(maxWidth: .infinity)
+
+                // フォロー, フォロワー, やっほう数
+                HStack(spacing: 24) {
+                    ForEach(myStatus, id: \.self) { status in
+                        VStack(spacing: 4) {
+                            Text("\(status.num)")
+                                .font(.system(size: 20, weight: .bold, design: .rounded))
+                            Text(status.title)
+                                .font(.system(size: 12, weight: .regular, design: .rounded))
+                        }
+                        if status == .follower || status == .following {
+                            Divider()
+                        }
+                    }
+                }
+                .frame(maxHeight: 50)
+
+                VStack(spacing: 12) {
+                    Text("最近交換したユーザー")
+                        .font(.system(size: 16, weight: .bold, design: .rounded))
+                        .frame(maxWidth: .infinity, alignment: .leading)
+
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack(spacing: 12) {
+                            partnerCard(cardImage: Image("nagano"), partner: PartnerInfo(name: "hotta mayu", age: 25, personality: "cute"))
+                            partnerCard(cardImage: Image("nagano2"), partner: PartnerInfo(name: "hotta mayu", age: 25, personality: "cute"))
+                            partnerCard(cardImage: Image("nagano3"), partner: PartnerInfo(name: "hotta mayu", age: 25, personality: "cute"))
+                            partnerCard(cardImage: Image("nagano4"), partner: PartnerInfo(name: "hotta mayu", age: 25, personality: "cute"))
+                            partnerCard(cardImage: Image("nagano5"), partner: PartnerInfo(name: "hotta mayu", age: 25, personality: "cute"))
+                            partnerCard(cardImage: Image("hotta"), partner: PartnerInfo(name: "hotta mayu", age: 25, personality: "cute"))
+                        }
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.top, 48)
             }
-            .frame(maxWidth: .infinity, alignment: .leading)
-            .padding(.top, 24)
         }
+        .fitToReadableContentGuide()
     }
 
     // MARK: - Card
