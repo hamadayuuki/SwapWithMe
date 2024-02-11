bbdfd4d1847786a0e78fb05ff64e0132427afd14
hamadayuuki <yuukirinnma@gmail.com>
2024-02-11 14:22:54 +0900

    Update request method

--- Sources/Data/Request/RelationshipRequest.swift
+++ Sources/Data/Request/RelationshipRequest.swift
@@ -14,22 +14,87 @@ import Relationship
 
 @DependencyClient
 public struct RelationshipRequest {
-    public var set: @Sendable (_ data: Relationship) throws -> Bool
+    public var set: @Sendable (_ myId: String, _ data: Relationship) throws -> Bool
+    public var addFollower: @Sendable (_ myId: String, _ friendId: String) async throws -> Bool
+    public var addFollowing: @Sendable (_ myId: String, _ friendId: String) async throws -> Bool
+    public var removeFollower: @Sendable (_ myId: String, _ friendId: String) async throws -> Bool
+    public var removeFollowing: @Sendable (_ myId: String, _ friendId: String) async throws -> Bool
+    public var fetch: @Sendable (_ id: String) async throws -> Relationship
 }
 
 // MARK: - DependencyKey
 
 extension RelationshipRequest: DependencyKey {
     public static let liveValue = Self(
-        set: { data in
-            guard let id = data.id else { fatalError("Error RelationshipRequest.set") }
+        set: { myId, data in
             let db = Firestore.firestore()
             do {
-                try db.collection("Relationship").document(id).setData(from: data)
+                try db.collection("Relationship").document(myId).setData(from: data)
                 return true
             } catch {
                 return false
             }
+        },
+        addFollower: { myId, friendId in
+            let db = Firestore.firestore()
+            let relationshipRef = db.collection("Relationship").document(myId)
+
+            do {
+                try await relationshipRef.updateData([
+                    "followersId": FieldValue.arrayUnion([friendId]),
+                    "updateAt": Timestamp(),
+                ])
+                return true
+            } catch {
+                return false
+            }
+        },
+        addFollowing: { myId, friendId in
+            let db = Firestore.firestore()
+            let relationshipRef = db.collection("Relationship").document(myId)
+
+            do {
+                try await relationshipRef.updateData([
+                    "followingsId": FieldValue.arrayUnion([friendId]),
+                    "updateAt": Timestamp(),
+                ])
+                return true
+            } catch {
+                return false
+            }
+        },
+        removeFollower: { myId, friendId in
+            let db = Firestore.firestore()
+            let relationshipRef = db.collection("Relationship").document(myId)
+
+            do {
+                try await relationshipRef.updateData([
+                    "followersId": FieldValue.arrayRemove([friendId]),
+                    "updateAt": Timestamp(),
+                ])
+                return true
+            } catch {
+                return false
+            }
+        },
+        removeFollowing: { myId, friendId in
+            let db = Firestore.firestore()
+            let relationshipRef = db.collection("Relationship").document(myId)
+
+            do {
+                try await relationshipRef.updateData([
+                    "followingsId": FieldValue.arrayRemove([friendId]),
+                    "updateAt": Timestamp(),
+                ])
+                return true
+            } catch {
+                return false
+            }
+        },
+        fetch: { id in
+            let db = Firestore.firestore()
+            let user = try await db.collection("Relationship").document(id).getDocument(as: Relationship.self)
+            return user
         }
     )
 
